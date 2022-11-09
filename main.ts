200cc5c342a346f2d03b4cafdded7508f87b741f
Naifff <knifffe@yandex.ru>
2022-11-09 13:56:28 +0300

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,50 @@
+input.onButtonPressed(Button.A, function () {
+    if (snake.get(LedSpriteProperty.Direction) == 90) {
+        snake.turn(Direction.Right, 90)
+    } else if (snake.get(LedSpriteProperty.Direction) == 180) {
+        snake.turn(Direction.Right, 90)
+    } else if (snake.get(LedSpriteProperty.Direction) == -90) {
+        snake.turn(Direction.Left, 90)
+    } else if (snake.get(LedSpriteProperty.Direction) == 0) {
+        snake.turn(Direction.Left, 90)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (snake.get(LedSpriteProperty.Direction) == 90) {
+        snake.turn(Direction.Left, 90)
+    } else if (snake.get(LedSpriteProperty.Direction) == 0) {
+        snake.turn(Direction.Right, 90)
+    } else if (snake.get(LedSpriteProperty.Direction) == -90) {
+        snake.turn(Direction.Right, 90)
+    } else if (snake.get(LedSpriteProperty.Direction) == 180) {
+        snake.turn(Direction.Left, 90)
+    }
+})
+function move () {
+    if (snake.get(LedSpriteProperty.X) == 4 && snake.get(LedSpriteProperty.Direction) == 90) {
+        snake.set(LedSpriteProperty.X, 0)
+    } else if (snake.get(LedSpriteProperty.X) == 0 && snake.get(LedSpriteProperty.Direction) == -90) {
+        snake.set(LedSpriteProperty.X, 4)
+    } else if (snake.get(LedSpriteProperty.Y) == 0 && snake.get(LedSpriteProperty.Direction) == 0) {
+        snake.set(LedSpriteProperty.Y, 4)
+    } else if (snake.get(LedSpriteProperty.Y) == 4 && snake.get(LedSpriteProperty.Direction) == 180) {
+        snake.set(LedSpriteProperty.Y, 0)
+    } else {
+        snake.move(1)
+    }
+    if (snake.isTouching(food)) {
+        game.addScore(1)
+        food.set(LedSpriteProperty.X, randint(0, 4))
+        food.set(LedSpriteProperty.Y, randint(0, 4))
+    }
+    basic.pause(600)
+}
+let food: game.LedSprite = null
+let snake: game.LedSprite = null
+snake = game.createSprite(2, 2)
+food = game.createSprite(randint(0, 4), randint(0, 4))
+food.set(LedSpriteProperty.Brightness, 100)
+game.startCountdown(10000)
+basic.forever(function () {
+    move()
+})
